f84daefbe0dd47283aec6eb7bd4680399e9c2310
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-06-21 19:01:40 -0700

    Adding a few serious updates

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,22 @@
 #![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 mod vga_buffer;
-
+mod serial;
 // We use no mange to disable name mangling, so that the output actually,
 // has the name _start, as opposed to K#JH$K28294.sd3.core$_fn.2392032
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
     println!("Kurogane OS will be with you shortly.");
-
+    println!("Keep tabs at")
     #[cfg(test)]
     test_main();
     // Extern "C" tells the compiler that it should use the C calling convention
@@ -35,6 +36,22 @@ pub extern "C" fn _start() -> ! {
 
     loop {}
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     // This function should never return,
@@ -46,4 +63,16 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+}
 #[test_case]
+fn trivial_assertion() {
+    serial_print!("Trivial assertion...");
+    assert_eq!(1, 1);
+    serial_println!("[ok]")
+}
