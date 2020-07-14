2dd08dcec5bbd4b6c3e2fd0df4096253e3cecbd0
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-07-13 19:12:08 -0700

    wrapping up here

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -44,6 +44,9 @@ lazy_static! {
 }
 extern "x86-interrupt" fn timer_interrupt_handler(stack_frame: &mut InterruptStackFrame) {
     print!(".");
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }
 pub fn init_idt() {
     IDT.load();
